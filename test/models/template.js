186e9a99cc625a7c6a4aa7f8a834feebed1ba602
Dejan Vasic <dejanvasic24@gmail.com>
2017-01-06 17:18:45 +1100

    Trying an integration test for the moment

---
+++ test/models/template.js
@@ -0,0 +1,27 @@
+import template from '../../src/models/template';
+
+describe('Template model', () => {
+
+	describe('create new template', () => {
+
+		it('saves a new docment', () => {
+
+			var templateDetails = new Template({
+				name: "hello there!"
+			});
+
+			return templateDetails.save().then((savedDocument) => {
+
+				expect(savedDocument).to.not.be.undefined;
+				expect(savedDocument.id).to.not.be.undefined;
+
+				console.log('savedDocument: ',savedDocument);
+				console.log('templateDetails: ',templateDetails);
+
+			});
+
+		});
+
+	});
+
+});
